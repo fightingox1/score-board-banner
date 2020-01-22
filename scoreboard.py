73beb3fb49c48bd351a9a2157d760dfab7caab20
fightingox1 <44482532+fightingox1@users.noreply.github.com>
2020-01-22 16:29:14 -0500

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -1,1 +1,24 @@
-
+import time
+score1 = 0
+score2 = 0
+tenthup = "false"
+loop = "true"
+while loop == "true":
+  count = "true"
+  while count == "true":
+    if tenthup == "true":
+      if score2 == 9:
+        score2 = 0
+        score1 = 0
+      else:
+        score1 = 0
+        score2 = score2 + 1
+      tenthup = "false"
+    score1 = score1 + 1
+    if score1 == 9:
+      tenthup = "true"
+    time.sleep(1)
+    count = "true"
+    
+    
+    
